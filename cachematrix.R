04c44a4a333603d221b96dd063923cc76142163e
Guy Woppelmann <guy.woppelmann@univ-lr.fr>
2014-08-20 14:17:05 +0200

    My solution to homework 2 of R-Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Week 3 Assignment. R Programming. August 2014
 
-## Write a short comment describing this function
+setwd("F:/GW/tmp/Tocopy/2_Rprog/homework2014")
 
+
+# Creates a special "matrix" object that can cache its inverse
+# The matrix is assumed to be invertible
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+    }
+    get <- function() x
+    setinverse <- function(inv) inverse <<- inv
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+# computes the inverse of the special "matrix" returned by makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$setinverse(inverse)
+    inverse
 }
+
+
+## GW. Tests inspired from discussion forum
+# a_matrix <- matrix(c(2,0,0,0,2,0,0,0,2), 3, 3)
+# a <- makeCacheMatrix(a_matrix)
+# a$get()            # Prints the above matrix
+# a$getinverse()     # Returns: NULL
+# cacheSolve(a)      # Computes inverse
+# a$getinverse()     # to show that the inverse has been stored and does not affect anything
+# cacheSolve(a)      # Restores the inverse from cached data
+
+
